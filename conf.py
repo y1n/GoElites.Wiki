9571051087fa83537d58514eec1df94ad73faabb
Yin <yinhavoc@gmail.com>
2018-03-15 00:12:32 +0200

    Fix

--- conf.py
+++ conf.py
@@ -1,13 +1,16 @@
-html_logo = 'img/ge_logo_small_outline.png'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+
 
 # Add any paths that contain custom static files (such as style sheets) here, 
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static', 'rtfd-css']
+
+# Logo and version display.
+html_logo = 'img/ge_logo_small_outline.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # Add custom .css files
 # https://github.com/snide/sphinx_rtd_theme/issues/117#issuecomment-41571653
